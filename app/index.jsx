8ea71c6a0fd3768d41b93ce6b8f5fb3cd881476f
sky2d <143280010+sky2d@users.noreply.github.com>
2024-08-08 16:56:25 +0530

    fix: Visibility of notification bar

--- app/index.jsx
+++ app/index.jsx
@@ -4,29 +4,31 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-black">
+    <SafeAreaView>
       <StatusBar
         hidden={false}
         backgroundColor="white"
         barStyle="dark-content"
       />
-      <View>
-        <Text className="text-sm text-white">
-          <Text className="text-primary font-bold">
-            Explore a world of connections!
+      <View className="min-h-screen items-center justify-center p-2 bg-black">
+        <View>
+          <Text className="text-sm text-white">
+            <Text className="text-primary font-bold">
+              Explore a world of connections !{" "}
+            </Text>
+            where every detail matters. Our app brings you closer to the people
+            who make the world vibrant, one profile at a time.
           </Text>
-          where every detail matters. Our app brings you closer to the people
-          who make the world vibrant, one profile at a time.
-        </Text>
-      </View>
-      <View>
-        <Link
-          className="mt-3 bg-primary p-2 rounded-xl hover:animate-pulse"
-          href="/user-details">
-          <Text className="text-white font-bold text-xl">
-            Go to user details
-          </Text>
-        </Link>
+        </View>
+        <View>
+          <Link
+            className="mt-3 bg-primary p-2 rounded-xl hover:animate-pulse"
+            href="/user-details">
+            <Text className="text-white font-bold text-xl">
+              Go to user details
+            </Text>
+          </Link>
+        </View>
       </View>
     </SafeAreaView>
   );
